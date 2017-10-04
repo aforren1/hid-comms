79af6928f1ffe11514df65c72142a6232536b03e
Alex Forrence <aforren1@jhu.edu>
2017-10-04 14:33:44 -0400

    Expectations

--- test_hid.py
+++ test_hid.py
@@ -12,21 +12,25 @@ sleep(0.3)
 res = dev.read(64)
 
 # should be ~ 6.14 (added 3 to the input)
+print('Expect ~ 6.14')
 print(struct.unpack('>f', bytearray(res[1:5])))
 
 ## char
 dev.write(b'x' + b'c' + b'y')
 sleep(0.3)
 res = dev.read(64)
+print('Expect y')
 print(chr(res[1])) # should be y
 
 ## short (int)
 dev.write(b'x' + b'i' + struct.pack('>h', 6))
 sleep(0.3)
 res = dev.read(64)
+print('Expect 18')
 print(struct.unpack('>h', bytearray(res[1:3])))
 
 ## long
+print('Expect 13270')
 dev.write(b'x' + b'l' + struct.pack('>l', 1327))
 sleep(0.3)
 res = dev.read(64)
--- test_libusb.py
+++ test_libusb.py
@@ -16,23 +16,27 @@ epo.write(pad(b'f' + struct.pack('>f', 3.14)))
 sleep(0.3)
 res = ep.read(ep.wMaxPacketSize)
 
+print('Expect ~6.14')
 print(struct.unpack('>f', res[1:5]))
 
 ## char
 epo.write(pad(b'c' + b'y'))
 sleep(0.3)
 res = ep.read(ep.wMaxPacketSize)
-
+print('Expect y')
 print(chr(res[1]))
 
 ## short (int)
 epo.write(pad(b'i' + struct.pack('>h', 6)))
 sleep(0.3)
 res = ep.read(64)
+print('Expect 18')
 print(struct.unpack('>h', res[1:3]))
 
 ## long
 epo.write(pad(b'l' + struct.pack('>l', 1327)))
 sleep(0.3)
 res = ep.read(64)
+print('Expect 13270')
 print(struct.unpack('>l', res[1:5]))
+
