306755ec50fa77e87d486b9a9affa0e7f5011294
Alex Forrence <aforren1@jhu.edu>
2017-10-09 11:30:54 -0400

    Blink LED on message

--- echo/echo.ino
+++ echo/echo.ino
@@ -1,19 +1,24 @@
 int n;
 int i;
 
+const int led_pin = 13;
+
 byte buffer_tx[64];
 byte buffer_rx[64];
 
 void setup() {
-  pinMode(13, OUTPUT); // set up to blink LED
+  pinMode(led_pin, OUTPUT); // set up to blink LED
 }
 
 void loop() {
   n = RawHID.recv(buffer_rx, 0);
   if (n > 0) {
+    digitalWrite(led_pin, HIGH);
     for (i = 0; i < 64; i++) {
       buffer_tx[i] = buffer_rx[i];
     }
     RawHID.send(buffer_tx, 0);
+    delay(200);
+    digitalWrite(led_pin, LOW);
   }
 }
